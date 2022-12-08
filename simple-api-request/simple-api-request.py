dd63ca207dbbccbefc7368cdf6008c795e87a898
Metin Durmus <mtn.drms@outlook.com>
2022-12-08 10:51:06 +0000

    tagging and new branch

--- simple-api-request/simple-api-request.py
+++ simple-api-request/simple-api-request.py
@@ -1,4 +1,7 @@
 import requests
 
+# Make changes for tagging
+# This is for the new branch
+
 response = requests.get("https://api.exchangerate.host/convert?from=USD&to=TRY")
 print(response.json())
